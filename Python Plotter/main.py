795e01f47f06d62610d8e88dd2fa2fa7a1cf642f
Adam Ghatta <67446944+RT5x@users.noreply.github.com>
2022-07-29 22:35:21 -0700

    Update main.py

--- Python Plotter/main.py
+++ Python Plotter/main.py
@@ -1,1 +1,12 @@
+from data import dvarset
+from data import ivarset
+import matplotlib.pyplot as plt
 
+y = dvarset
+x = ivarset
+
+plt.plot(x,y)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("X effect on Y")
+plt.show()
