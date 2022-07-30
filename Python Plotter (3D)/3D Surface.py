b89c49131ee6a92cc748156a72406a607b3d0074
Adam Ghatta <67446944+RT5x@users.noreply.github.com>
2022-07-30 07:55:04 -0700

    Update 3D Surface.py

--- Python Plotter (3D)/3D Surface.py
+++ Python Plotter (3D)/3D Surface.py
@@ -1,1 +1,17 @@
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+ 
 
+#dataset
+x = np.outer(np.linspace(-3, 3, 32), np.ones(32))
+y = x.copy().T
+z = (np.sin(x **2) + np.cos(y) )
+ 
+fig = plt.figure(figsize =(14, 9))
+ax = plt.axes(projection ='3d')
+ 
+ax.plot_surface(x, y, z)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
