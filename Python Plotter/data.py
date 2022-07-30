210c75df85e4d873bea32ef224c383e290616d1a
Adam Ghatta <67446944+RT5x@users.noreply.github.com>
2022-07-29 22:35:06 -0700

    Create data.py

---
+++ Python Plotter/data.py
@@ -0,0 +1,5 @@
+ivars = "1 2 3 4 5 6 7 8 9 10"
+dvars = "3.867278 8.19849 0.8278 9.28718 10.287382 11.38738 1.292989 8.3873 2.873 12.8377"
+
+ivarset = [float(num) for num in ivars.split()]
+dvarset = [float(num) for num in dvars.split()]
